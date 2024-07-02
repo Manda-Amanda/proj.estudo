fe81b248a50ca08187fe4d1721f4cb49e623173b
Amanda Alves <alvesdasilvaamanda021@gmail.com>
2024-07-01 22:15:03 -0300

    projeto  de estudo

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bem vindo");
+        System.out.println("Filme:Top Gun");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de Lançamento:" + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        /// Média calculada pelas 3 notas
+        double media = (9.8 + 6.3 + 8.0) /3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = "Filme de aventura ";
+        System.out.println(sinopse);
+
+        int classificacao;
+        classificacao = (int) (media /2);
+        System.out.println(classificacao);
+
+    }
+}
